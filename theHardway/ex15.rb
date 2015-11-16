8b4aa8c641d979b36d203a564623d5ecc93de3e6
Mark Thomas <markthethomas@gmail.com>
2015-11-16 11:13:27 -0800

    add better checks in file history cli thing

--- theHardway/ex15.rb
+++ theHardway/ex15.rb
@@ -1,6 +1,7 @@
 filename = ARGV.first
 
-if !File.exist?(filename)
+# Check to see if the file exists or is a zero-sized file
+if !File.file?(filename) || !File.size?(filename)
   puts "File does not exist"
   exit
 end
@@ -10,4 +11,6 @@ file = open(filename)
 puts "Here is the file's history"
 puts "=" * 20
 puts "Created at #{file.birthtime}"
+puts "Last modified at #{File.ctime(file)}"
+puts "Last accessed at #{File.atime(file)}"
 puts "Extension type: #{File.extname(file)} "
